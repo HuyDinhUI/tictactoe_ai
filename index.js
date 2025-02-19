c3c8a627048d95877314ddf97d09210a64be06f7
HuyDinhUI <146050567+HuyDinhUI@users.noreply.github.com>
2025-02-19 12:57:42 +0700

    change portport

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ socketIo.on("connection", (socket) => {
   });
 });
 
-const PORT = 4000;
+const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
